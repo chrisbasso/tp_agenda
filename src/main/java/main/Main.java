5c79e4496f198a94c059826e71f76734098c04c4
@gisbert.ra <gisbert.ra@gmail.com>
2019-03-18 23:20:52 -0300

    mysql: crear datos

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -13,10 +13,11 @@ public class Main
 	public static void main(String[] args) 
 	{
 		//Mysql.start();
+		//Mysql.loadInitialData();
 		Vista vista = new Vista();
 		Agenda modelo = new Agenda(new DAOSQLFactory());
 		Controlador controlador = new Controlador(vista, modelo);
 		controlador.inicializar();
-		
+	
 	}
 }
